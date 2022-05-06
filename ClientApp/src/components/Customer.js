44ff7a32a662ac6af10ae87f91196909318d2bda
YeinsM <wildermancera1@gmail.com>
2022-05-06 13:43:25 -0400

    join FrontBack

---
+++ ClientApp/src/components/Customer.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { CustomerTable } from "./helpers/Table";
+
+export const Customer = () => {
+  return(
+    <CustomerTable/>
+  )
+};
+
+export default Customer;
